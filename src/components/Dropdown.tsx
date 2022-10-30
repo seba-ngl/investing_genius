621768310662360b34996e877c0ad94b61e8cdc0
Sebastian Niguleasa <seba80443@gmail.com>
2022-10-30 14:32:21 +0200

    Close dropdown on outside click

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./dropdown.style.scss";
 
 const Dropdown = () => {
+  const wrapperRef = useRef<any>(null);
   const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
 
   return (
     <div className="dropdown">
       <button
+        ref={wrapperRef}
         type="button"
         className="dropdown-toggle"
         tabIndex={0}

0add03e678a05a4e33ea52abea5c78704124f5f7
Sebastian Niguleasa <seba80443@gmail.com>
2022-11-05 19:31:44 +0200

    Modify breadcrumbs

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -1,6 +1,43 @@
-const BreadCrumbs = () => {
-  //TODO: Continue breadcrumbs
-  return <div>BreadCrumbs</div>;
+import { Link } from "react-router-dom";
+import ArrowSeparator from "./ArrowSeparator";
+import "./breadCrumbs.style.scss";
+
+type IProps = {
+  links?: string[];
+};
+
+const BreadCrumbs = ({ links }: IProps) => {
+  const capitalizeFirstLetter = (text: string) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  };
+
+  const capitalizeAllLetters = (text: string) => {
+    return text.toUpperCase();
+  };
+
+  return (
+    <nav className="breadcrumbs-comp">
+      {links?.map((link, index) =>
+        index !== links.length - 1 && index !== 0 ? (
+          <>
+            <Link className="link" to={"/" + link}>
+              {capitalizeFirstLetter(link)}
+            </Link>
+            <ArrowSeparator />
+          </>
+        ) : index === 0 ? (
+          <>
+            <Link className="link" to={"/"}>
+              {capitalizeFirstLetter(link)}
+            </Link>
+            <ArrowSeparator />
+          </>
+        ) : (
+          <div>{capitalizeAllLetters(link)}</div>
+        )
+      )}
+    </nav>
+  );
 };
 
 export default BreadCrumbs;
